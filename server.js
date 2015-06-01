97b420bfce0e73cb12d850eec2dce90f521c3847
Asif Ahmed <asif@parksleepfly.com>
2015-06-01 12:14:52 -0700

    changes for heroku

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ var app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+// public folder to store assets
 app.use(express.static(__dirname + '/public'));
 
+// routes for app
 app.get('/', function(req, res) {
   res.render('pad', {padName: ""});
 });
@@ -14,13 +16,21 @@ app.get('/(:id)', function(req, res) {
 });
 
 // get sharejs dependencies
-var redis = require('redis');
 var sharejs = require('share');
+var redis;
+if (process.env.REDISTOGO_URL) {
+	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+	var redis = require("redis").createClient(rtg.port, rtg.hostname);
+	redis.auth(rtg.auth.split(":")[1]);
+} else {
+  redis = require("redis").createClient();
+}
 
+// options for sharejs 
 var options = {
-  db: {type: 'redis'},
+  client: redis,
   auth: function(client, action) {
-    // This auth handler rejects any ops bound for docs starting with 'readonly'.
+    // this auth handler rejects any ops bound for docs starting with '' (home page)
     if (action.name === 'submit op' && action.docName === "") {
       action.reject();
     } else {
@@ -29,13 +39,11 @@ var options = {
   }
 };
 
+// attach the express server to sharejs
 sharejs.server.attach(app, options);
 
-var server = app.listen(8000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+// listen on port 8000 (for localhost) or the port defined for heroku
+app.set('port', (process.env.PORT || 8000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
