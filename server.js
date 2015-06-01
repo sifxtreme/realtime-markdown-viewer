098bbda6422aef20024a8d055a77a0811924571d
Asif Ahmed <asif@parksleepfly.com>
2015-06-01 12:21:18 -0700

    fix config

--- server.js
+++ server.js
@@ -18,9 +18,10 @@ app.get('/(:id)', function(req, res) {
 // get sharejs dependencies
 var sharejs = require('share');
 var redis;
+console.log(process.env.REDISTOGO_URL);
 if (process.env.REDISTOGO_URL) {
 	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-	var redis = require("redis").createClient(rtg.port, rtg.hostname);
+	redis = require("redis").createClient(rtg.port, rtg.hostname);
 	redis.auth(rtg.auth.split(":")[1]);
 } else {
   redis = require("redis").createClient();
