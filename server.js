ad1983c9ef7b4f7d25388321f3a9a8fbd561ad7a
Asif Ahmed <asif@parksleepfly.com>
2015-06-10 19:27:23 -0700

    break out html file into css / js files

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/* server.js */
+
 var express = require('express');
 var app = express();
 
@@ -9,10 +11,10 @@ app.use(express.static(__dirname + '/public'));
 
 // routes for app
 app.get('/', function(req, res) {
-  res.render('pad', {padName: ""});
+  res.render('pad');
 });
 app.get('/(:id)', function(req, res) {
-  res.render('pad', {padName: req.params.id});
+  res.render('pad');
 });
 
 // get sharejs dependencies
@@ -22,31 +24,21 @@ var sharejs = require('share');
 var redisClient;
 console.log(process.env.REDISTOGO_URL);
 if (process.env.REDISTOGO_URL) {
-	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-	redisClient = require("redis").createClient(rtg.port, rtg.hostname);
-	redisClient.auth(rtg.auth.split(":")[1]);
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+  redisClient.auth(rtg.auth.split(":")[1]);
 } else {
   redisClient = require("redis").createClient();
 }
 
 // options for sharejs 
 var options = {
-  db: {type: 'redis', client: redisClient},
-  auth: function(client, action) {
-    // this auth handler rejects any ops bound for docs starting with '' (home page)
-    if (action.name === 'submit op' && action.docName === "") {
-      action.reject();
-    } else {
-      action.accept();
-    }
-  }
+  db: {type: 'redis', client: redisClient}
 };
 
 // attach the express server to sharejs
 sharejs.server.attach(app, options);
 
 // listen on port 8000 (for localhost) or the port defined for heroku
-app.set('port', (process.env.PORT || 8000));
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+var port = process.env.PORT || 8000;
+app.listen(port);
